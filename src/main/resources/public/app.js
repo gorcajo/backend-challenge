23730d6a9fa47390dfeb1ba8e93b3d5a39dfca66
Guille <mr.guiller@gmail.com>
2019-06-30 00:13:16 +0200

    updated to react

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -1,103 +1,102 @@
-//GLOB = {
-//	basketId : null,
-//
-//	init : function() {
-//		"use strict";
-//
-//		document.getElementById("btn-empty").onclick = function() {
-//			GLOB.deleteBasket(GLOB.basketId);
-//		};
-//
-//		document.getElementById("btn-add-1").onclick = function() {
-//			GLOB.addProductToBasket(GLOB.basketId, "VOUCHER");
-//		};
-//
-//		document.getElementById("btn-add-2").onclick = function() {
-//			GLOB.addProductToBasket(GLOB.basketId, "TSHIRT");
-//		};
-//
-//		document.getElementById("btn-add-3").onclick = function() {
-//			GLOB.addProductToBasket(GLOB.basketId, "MUG");
-//		};
-//
-//		GLOB.createNewBasket();
-//	},
-//
-//	createNewBasket : function() {
-//		let ajax = new XMLHttpRequest();
-//
-//		ajax.open("POST", "/api/v1/basket/", true);
-//		ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//
-//		ajax.onreadystatechange = function() {
-//			if (this.readyState === 4) {
-//				if (this.status === 201) {
-//					GLOB.basketId = this.responseText;
-//					document.getElementById("total-amount").innerHTML = "0 €";
-//				}
-//				else {
-//					alert("HTTP " + this.status);
-//				}
-//			}
-//		};
-//
-//		ajax.send();
-//	},
-//
-//	addProductToBasket : function(id, productCode) {
-//		let ajax = new XMLHttpRequest();
-//
-//		ajax.open("PUT", "/api/v1/basket/" + id, true);
-//		ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//
-//		ajax.onreadystatechange = function() {
-//			if (this.readyState === 4) {
-//				if (this.status === 204)
-//					GLOB.refreshTotalAmount(id);
-//				else
-//					GLOB.createNewBasket();
-//			}
-//		};
-//
-//		ajax.send(productCode);
-//	},
-//
-//	refreshTotalAmount : function(id) {
-//		let ajax = new XMLHttpRequest();
-//
-//		ajax.open("GET", "/api/v1/basket/" + id + "/totalamount", true);
-//		ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//
-//		ajax.onreadystatechange = function() {
-//			if (this.readyState === 4) {
-//				if (this.status === 200)
-//					document.getElementById("total-amount").innerHTML = parseFloat(this.responseText).toFixed(2) + " €";
-//				else
-//					GLOB.createNewBasket();
-//			}
-//		};
-//
-//		ajax.send();
-//	},
-//
-//	deleteBasket : function(id) {
-//		let ajax = new XMLHttpRequest();
-//
-//		ajax.open("DELETE", "/api/v1/basket/" + id, true);
-//		ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//
-//		ajax.onreadystatechange = function() {
-//			if (this.readyState === 4)
-//				GLOB.createNewBasket();
-//		};
-//
-//		ajax.send();
-//	},
-//};
-//
-//GLOB.init();
+class Application extends React.Component {
+	
+	constructor() {
+		super();
+		
+		this.state = {
+			basketId: null,
+			totalAmount: 0.0,
+		};
 
-class Products extends React.Component {
+		this.createNewBasket = this.createNewBasket.bind(this);
+		this.addProductToBasket = this.addProductToBasket.bind(this);
+		this.refreshTotalAmount = this.refreshTotalAmount.bind(this);
+		this.deleteBasket = this.deleteBasket.bind(this);
+	}
+	
+	componentDidMount() {
+		this.createNewBasket();
+	}
+	
+	createNewBasket() {
+		fetch("/api/v1/basket", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json;charset=UTF-8"
+			}
+		})
+		.then(response => response.text())
+		.then((data) => {
+			this.setState({basketId: data})
+		});
+	}
+
+	addProductToBasket(productCode) {
+		fetch("/api/v1/basket/" + this.state.basketId, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json;charset=UTF-8"
+			},
+			body: productCode
+		})
+		.then(() => {
+			this.refreshTotalAmount();
+		});
+	}
+
+	refreshTotalAmount() {
+		fetch("/api/v1/basket/" + this.state.basketId + "/totalamount", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json;charset=UTF-8"
+			}
+		})
+		.then(response => response.text())
+		.then((data) => {
+			this.setState({
+				totalAmount: data
+			});
+		});
+	}
+	
+	deleteBasket() {
+		fetch("/api/v1/basket/" + this.state.basketId, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json;charset=UTF-8"
+			}
+		})
+		.then(() => {
+			this.createNewBasket();
+		});
+	}
+	
+	renderBasket(totalAmount) {
+		return <Basket onBasketDelete={this.deleteBasket} totalAmount={this.state.totalAmount}/>;
+	}
+	
+	render() {
+		return (
+		    <div>
+			    <div className='row'>
+				    <div className='col-md-12'>
+				        <div className='header'>
+				            <h1>Backend Challenge</h1>
+				            <h4><i>Guillermo Orcajo García</i></h4>
+				        </div>
+				    </div>
+				</div>
+				
+			    <div className='row'>
+			        <ProductsArea onProductAdd={this.addProductToBasket} />
+			        {this.renderBasket()}
+			    </div>
+		    </div>
+		);
+	}
+}
+
+class ProductsArea extends React.Component {
 
     render() {
         return (
@@ -108,31 +107,28 @@ class Products extends React.Component {
                     <div className='card-body'>
                         <div className='row'>
                             <div className='col-md-4'>
-                                <div className='card'>
-                                    <div className='card-header product-header'>Cabify Voucher</div>
-                                    <div className='card-body'>
-                                        <p className='product-amount'>5.00 €</p>
-                                    <button id='btn-add-1' type='button' className='btn btn-success btn-block'>Add to basket</button>
-                                    </div>
-                                </div>
+                                <Product
+                                	code={"VOUCHER"}
+                                	name={"Cabify Voucher"}
+                                	price={5.0}
+                                	onProductAdd={this.props.onProductAdd}
+                                />
                             </div>
                             <div className='col-md-4'>
-                                <div className='card'>
-                                    <div className='card-header product-header'>Cabify T-Shirt</div>
-                                    <div className='card-body'>
-                                        <p className='product-amount'>20.00 €</p>
-                                        <button id='btn-add-2' type='button' className='btn btn-success btn-block'>Add to basket</button>
-                                    </div>
-                                </div>
+                            	<Product
+	                            	code={"TSHIRT"}
+	                            	name={"Cabify T-Shirt"}
+	                            	price={20.0}
+	                            	onProductAdd={this.props.onProductAdd}
+                            	/>
                             </div>
                             <div className='col-md-4'>
-                                <div className='card'>
-                                    <div className='card-header product-header'>Cabify Coffee Mug</div>
-                                    <div className='card-body'>
-                                        <p className='product-amount'>7.50 €</p>
-                                        <button id='btn-add-3' type='button' className='btn btn-success btn-block'>Add to basket</button>
-                                    </div>
-                                </div>
+                        		<Product
+	                        		code={"MUG"}
+	                        		name={"Cabify Coffee Mug"}
+	                        		price={7.5}
+	                        		onProductAdd={this.props.onProductAdd}
+                        		/>
                             </div>
                         </div>
                     </div>
@@ -142,9 +138,41 @@ class Products extends React.Component {
     }
 }
 
+class Product extends React.Component {
+	
+    render() {
+    	let code = this.props.code;
+    	let name = this.props.name;
+    	let price = parseFloat(this.props.price).toFixed(2);
+    	
+        return (
+            <div className='card'>
+                <div className='card-header product-header'>{name}</div>
+                <div className='card-body'>
+                    <p className='product-amount'>{price} €</p>
+                    <AddToBasketButton onClick={() => this.props.onProductAdd(code) } />
+                </div>
+            </div>
+        );
+    }
+}
+
+class AddToBasketButton extends React.Component {
+	
+	render() {
+		return (
+            <button type='button' className='btn btn-success btn-block' onClick={this.props.onClick}>
+            	Add to basket
+            </button>
+		);
+	}
+}
+
 class Basket extends React.Component {
 
     render() {
+    	let totalAmount = parseFloat(this.props.totalAmount).toFixed(2);
+    	
         return (
             <div className='col-md-3 offset-md-1'>
                 <div className='card'>
@@ -153,13 +181,13 @@ class Basket extends React.Component {
                     <div className='card-body'>
                         <div className='row'>
                             <div className='col-md-12'>
-                                <p id='total-amount' className='basket-amount'>0 €</p>
+                                <p id='total-amount' className='basket-amount'>{totalAmount} €</p>
                             </div>
                         </div>
                         
                         <div className='row'>
                             <div className='col-md-12'>
-                                <button id='btn-empty' type='button' className='btn btn-danger btn-block'>Empty basket</button>
+                            	<DeleteBasketButton onClick={this.props.onBasketDelete} />
                             </div>
                         </div>
                     </div>
@@ -169,24 +197,20 @@ class Basket extends React.Component {
     }
 }
 
+class DeleteBasketButton extends React.Component {
+	
+	render() {
+		return (
+            <button type='button' className='btn btn-danger btn-block' onClick={this.props.onClick}>
+            	Empty basket
+            </button>
+		);
+	}
+}
+
 // ========================================
 
 ReactDOM.render(
-    <div>
-	    <div className='row'>
-		    <div className='col-md-12'>
-		        <div className='header'>
-		            <h1>Backend Challenge</h1>
-		            <h4><i>Guillermo Orcajo García</i></h4>
-		        </div>
-		    </div>
-		</div>
-		
-	    <div className='row'>
-	        <Products />
-	        <Basket />
-	    </div>
-    </div>,
+	<Application />,
     document.getElementById('react-root')
 );
-
